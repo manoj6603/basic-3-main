53bada440db3f6d13f726704dc5ca398b33d2bd1
manoj6603 <kurichetimanojkumar@gmail.com>
2024-02-16 13:30:53 +0530

    final commit

---
+++ Handwritten-Digit-Recognition-using-Deep-Learning-main/MNIST_Dataset_Loader/dataset/ReadMe.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5ff3db2d8e645ecfa19d8404e0da274194ca7329450e856e98fa722c26e42528
+size 252
---
+++ Handwritten-Digit-Recognition-using-Deep-Learning-main/MNIST_Dataset_Loader/mnist_loader.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:01a5d4a81745fb5db86c10955911d8151ad1f38c41e6be0ef2a1a5ad2ffc0a40
+size 2296
